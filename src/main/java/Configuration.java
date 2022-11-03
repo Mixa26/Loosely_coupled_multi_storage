3c64c1f5a5d5c85dc94262c123db896310d5284c
Mihajlo <djmixa26.hockey@gmail.com>
2022-11-03 14:47:05 +0100

    Changed json dependencies because the old ones were thrash, deleted error folder because modules shouldnt use print statements, exceptions will be implemented.

--- src/main/java/Configuration.java
+++ src/main/java/Configuration.java
@@ -1,22 +1,9 @@
-import com.github.cliftonlabs.json_simple.JsonKey;
-import com.github.cliftonlabs.json_simple.JsonObject;
-import com.github.cliftonlabs.json_simple.Jsonable;
-import com.github.cliftonlabs.json_simple.Jsoner;
-
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
-
-import java.io.IOException;
-import java.io.StringWriter;
-import java.io.Writer;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import java.util.ArrayList;
-import java.util.Iterator;
+import java.util.List;
 
-import static java.lang.Math.toIntExact;
-
-public class Configuration implements Jsonable {
+public class Configuration{
     private long bytes;
     private int files;
     private ArrayList<String> forbiddenExtensions;
@@ -34,63 +21,23 @@ public class Configuration implements Jsonable {
         this.forbiddenExtensions = forbiddenExtensions;
     }
 
-    @Override
-    public String toJson () {
-        /**
-         * Calls toJson with writer argument and returns
-         * a string containing json data of the Configuration
-         * object it is called on.
-         */
-        final StringWriter writable = new StringWriter();
-        try {
-            this.toJson(writable);
-        } catch (final IOException e) {
-            System.out.println("Json problem in class Configuration!");
-        }
-        return writable.toString();
+    public String toJson(ArrayList<Configuration> arrayList) throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+
+        // Java object to JSON string
+        return mapper.writeValueAsString(arrayList);
     }
 
-    @Override
-    public void toJson (Writer writer) throws IOException {
+    public List<Configuration> fromJson(String json) throws JsonProcessingException {
         /**
-         * Is called by toJson with no arguments
-         * and fills the writer with json data
+         * Returns a List< Configuration >, USE THIS ONLY WITH A JSON CONTAINING LIST < CONFIGURATION >
          */
-        final JsonObject json = new JsonObject();
-        json.put("bytes", this.getBytes());
-        json.put("files", this.getFiles());
-        json.put("forbiddenExtensions", this.getForbiddenExtensions());
-        json.toJson(writer);
-    }
+        ArrayList<Configuration> arrayList = new ArrayList<>();
 
-    public Configuration fromJson (String fileText)
-    {
-        /**
-         * Grabs json data from a string text and
-         * transforms is into a Configuration object
-         * which is also returned by this function.
-         */
-        try {
-            JSONParser jsonParser = new JSONParser();
-            JSONObject obj = (JSONObject) jsonParser.parse(fileText);
-            Configuration config = new Configuration();
+        ObjectMapper mapper = new ObjectMapper();
 
-            config.setBytes((long) obj.get("bytes"));
-            config.setFiles(toIntExact((long) obj.get("files")));
-            JSONArray jsonArray = (JSONArray) obj.get("forbiddenExtensions");
+        return  (List<Configuration>) mapper.readValue(json, List.class);
 
-            Iterator<String> iterator = jsonArray.iterator();
-            ArrayList<String> forbiddenExtensions = new ArrayList<>();
-            while (iterator.hasNext()) {
-                forbiddenExtensions.add(iterator.next());
-            }
-            config.setForbiddenExtensions(forbiddenExtensions);
-
-            return config;
-        } catch (ParseException e) {
-            System.out.println("Jason parse exception!");
-        }
-        return null;
     }
 
     public long getBytes() {
